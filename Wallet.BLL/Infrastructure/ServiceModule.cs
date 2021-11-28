6090ca186814bdee8636bdacfb73a20d2e008c5f
BorysShulyak <borysshulyak@gmail.com>
2021-11-28 20:18:38 +0200

    Implement ServiceModule using Ninject.

---
+++ Wallet.BLL/Infrastructure/ServiceModule.cs
@@ -0,0 +1,19 @@
+﻿using Ninject.Modules;
+using Wallet.DAL.Interfaces;
+using Wallet.DAL.Repositories;
+
+namespace Wallet.BLL.Infrastructure
+{
+    public class ServiceModule : NinjectModule
+    {
+        private string connectionString;
+        public ServiceModule(string connection)
+        {
+            connectionString = connection;
+        }
+        public override void Load()
+        {
+            Bind<IUnitOfWork>().To<EFUnitOfWork>().WithConstructorArgument(connectionString);
+        }
+    }
+}
