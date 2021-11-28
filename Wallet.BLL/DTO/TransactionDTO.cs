ff29246727014e6a1b6f0b2a33ba158ed0f55bfa
BorysShulyak <borysshulyak@gmail.com>
2021-11-28 19:48:22 +0200

    Implement ValidationException.

---
+++ Wallet.BLL/DTO/TransactionDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Wallet.BLL.DTO
+{
+    public class TransactionDTO
+    {
+        public int Id { get; set; }
+        public decimal MoneySum { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+
+        public int SourceAccountId { get; set; }
+
+        public int TargetAccountId { get; set; }
+    }
+}
