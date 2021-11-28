858c8571b39f83556fbf325abfe6a76268e00c58
BorysShulyak <borysshulyak@gmail.com>
2021-11-28 19:41:05 +0200

    Implement AccountDTO.

---
+++ Wallet.BLL/DTO/AccountDTO.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Wallet.BLL.DTO
+{
+    public class AccountDTO
+    {
+        public int Id { get; set; }
+        public decimal Money { get; set; }
+    }
+}
--- Wallet.DAL/Entities/Transaction.cs
+++ Wallet.DAL/Entities/Transaction.cs
@@ -6,6 +6,7 @@ namespace Wallet.DAL.Entities
         public int Id { get; set; }
         public decimal MoneySum { get; set; }
         public DateTime Date { get; set; }
+        public string Description { get; set; }
 
         public int SourceAccountId { get; set; }
         public Account SourceAccount { get; set; }
