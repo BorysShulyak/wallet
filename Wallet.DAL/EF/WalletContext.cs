a1cb147a00d3b6a97e09c10869d5c10218a86035
BorysShulyak <borysshulyak@gmail.com>
2021-11-28 19:03:59 +0200

    Add WalletContext.

---
+++ Wallet.DAL/EF/WalletContext.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using Wallet.DAL.Entities;
+
+namespace Wallet.DAL.EF
+{
+    public class WalletContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Currency> Currencies { get; set; }
+
+        static WalletContext()
+        {
+            Database.SetInitializer<WalletContext>(new StoreDbInitializer());
+        }
+        public WalletContext(string connectionString)
+            : base(connectionString)
+        {
+        }
+    }
+
+    public class StoreDbInitializer : DropCreateDatabaseIfModelChanges<WalletContext>
+    {
+        protected override void Seed(WalletContext db)
+        {
+            db.Currencies.Add(new Currency { Name = "USD" });
+            db.Currencies.Add(new Currency { Name = "UAH" });
+
+            db.Accounts.Add(new Account { Money = 1000 });
+            db.Accounts.Add(new Account { Money = 1000 });
+
+            db.SaveChanges();
+        }
+    }
+}
